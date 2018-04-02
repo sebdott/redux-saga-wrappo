c87b8eacff1af1115ca11434dccc5b018d5fc119
sebastian <sebastian-jx@jxinternet.com>
2018-04-02 18:09:29 +0800

    Commit

--- index.js
+++
@@ -1,7 +0,0 @@
-export * from './lib/connect';
-export * from './lib/addOnActionTypeNaming';
-export * from './lib/generateReducers';
-export * from './lib/generateSagas';
-export * from './lib/registerComponent';
-export * from './lib/appCreator';
-
